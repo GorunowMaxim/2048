83f477faa4042f36805964de516ffca90216379d
GorunowMaxim <mgorunow@gmail.com>
2024-09-07 00:59:38 +0400

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react()],
+	base: '/2048',
 	resolve: {
-		alias: [{ find: '~', replacement: resolve(__dirname, './src') }],
+		alias: {
+			app: './src/app',
+			entities: './src/entities',
+			widgets: './src/widgets',
+		},
 	},
 });
